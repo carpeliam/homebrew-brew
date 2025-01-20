86412c32a6e6475e99cc33e4b9ad8689505ddad2
Liam Morley <liam@carpeliam.com>
2025-01-20 18:23:57 -0500

    Update gitshorty to v0.0.2

--- Formula/gitshorty.rb
+++ Formula/gitshorty.rb
@@ -1,8 +1,8 @@
 class Gitshorty < Formula
   desc "Shortcut command-line tool"
   homepage "https://github.com/carpeliam/gitshorty"
-  url "https://github.com/carpeliam/gitshorty/archive/refs/tags/v0.0.1.tar.gz"
-  sha256 "3f1012d231789cf5519867b8c735930c7c3547c2bbcdc4bca4006d14742ee732"
+  url "https://github.com/carpeliam/gitshorty/archive/refs/tags/v0.0.2.tar.gz"
+  sha256 "3a707834804bdae1ae657f94055f6f4a0dd86119b1bb8d94d9694fd417cd2039"
   license "MIT"
 
   depends_on "go" => :build
